61102c6064176f842326b52103dea1bb5694202b
bastgood3166666 <sansan197166666@gmail.com>
2025-10-18 02:07:57 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["139.95.9.185"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["8.138.8.80"];
 pub const RS_PUB_KEY: &str = "gRP1LZ3P4dMEatpfCA4qfNVTETTCmmn7cPkK+uX3HVk=";
 pub const RENDEZVOUS_PORT: i32 = 40676;
 pub const RELAY_PORT: i32 = 40677;
